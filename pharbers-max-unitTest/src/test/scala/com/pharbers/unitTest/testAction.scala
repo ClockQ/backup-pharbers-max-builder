3308910153565f6e74fd249623b8f6ac22ffbd6b
clock <2567729201@qq.com>
2019-02-14 15:31:25 +0800

    servier over

--- pharbers-max-unitTest/src/test/scala/com/pharbers/unitTest/testAction.scala
+++ pharbers-max-unitTest/src/test/scala/com/pharbers/unitTest/testAction.scala
@@ -12,11 +12,11 @@ object testAction extends App {
 //    val actionLst = generateNameAction("max_json/tmp.json").toSingleList
 //    val actionLst = generateNameAction("max_json/nhwa-mz-1804.json", "max_json/tmp-nhwa-mz-1804.json").toSingleList
 //    val actionLst = generateNameAction("max_json/astellas-all-1804.json", "max_json/tmp-astellas-all-1804.json").toSingleList
-    val actionLst = generateNameAction("max_json/tq-rp-1806.json", "max_json/tmp-tq-rp-1806.json").toSingleList
+//    val actionLst = generateNameAction("max_json/tq-rp-1806.json", "max_json/tmp-tq-rp-1806.json").toSingleList
 //    val actionLst = generateNameAction("max_json/tq-sa-1806.json", "max_json/tmp-tq-sa-1806.json").toSingleList
 //    val actionLst = generateNameAction("max_json/bayer-all-1811.json", "max_json/tmp-bayer-all-1811.json").toSingleList
 //    val actionLst = generateNameAction("max_json/xlt-all-1811.json", "max_json/tmp-xlt-all-1811.json").toSingleList
-//    val actionLst = generateNameAction("max_json/servier-all-1806.json", "max_json/tmp-servier-all-1806.json").toSingleList
+    val actionLst = generateNameAction("max_json/servier-all-1806.json", "max_json/tmp-servier-all-1806.json").toSingleList
 //    val actionLst = generateNameAction("max_json/pfizer-all-1804.json", "max_json/tmp-pfizer-all-1806.json").toSingleList
 //    val actionLst = generateNameAction("max_json/qilu-all-1809.json", "max_json/tmp-qilu-all-1809.json").toSingleList
 //    val actionLst = generateNameAction("max_json/bt-all-1809.json", "max_json/tmp-bt-all-1809.json").toSingleList
