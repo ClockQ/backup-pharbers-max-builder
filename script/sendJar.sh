b6a2448531c001d86a03827bab5abf9b1eaa3ea3
clock <2567729201@qq.com>
2019-11-29 23:35:02 +0800

    写了个小脚本

--- script/sendJar.sh
+++ script/sendJar.sh
@@ -1,5 +1,6 @@
 # /bin/bash
 
+# 发布本地依赖脚本
 ssh spark@max.logic "mkdir -p /Users/clock/workSpace/Pharbers/pharbers-max-builder/pharbers-max-astellas/target/"
 scp -r pharbers-max-astellas/target/pharbers-max-astellas-1.0.jar spark@max.logic:/Users/clock/workSpace/Pharbers/pharbers-max-builder/pharbers-max-astellas/target/pharbers-max-astellas-1.0.jar
 
@@ -12,3 +13,8 @@ scp -r pharbers-max-pfizer/target/pharbers-max-pfizer-1.0.jar spark@max.logic:/U
 ssh spark@max.logic "mkdir -p /Users/clock/workSpace/Pharbers/pharbers-max-builder/pharbers-max-servier/target/"
 scp -r pharbers-max-servier/target/pharbers-max-servier-1.0.jar spark@max.logic:/Users/clock/workSpace/Pharbers/pharbers-max-builder/pharbers-max-servier/target/pharbers-max-servier-1.0.jar
 
+scp -r pharbers-max-main/target/lib spark@max.logic:/home/spark/max_driver/
+scp -r pharbers-max-main/target/pharbers-max-main-1.0.jar spark@max.logic:/home/spark/max_driver/
+
+# 启动命令
+# nohup java -jar pharbers-max-main-1.0.jar 1>output.log 2>&1 &
